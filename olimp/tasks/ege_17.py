3f954d5dfa3fdba95c1f4041fa251da92aaee8b2
lot <lotar73@gmail.com>
2022-04-14 15:25:37 +0300

    переделка 02_tk.py

--- olimp/tasks/ege_17.py
+++ olimp/tasks/ege_17.py
@@ -1,24 +1,37 @@
 # Задание 17 № 37356
 
 f = open("17.txt")
-arr = f.readlines()
-print(arr[:20])
+a = [int(i) for i in f]
 
-a = [int(string[:-1]) for string in arr]
-a = []
-for string in arr:
-    a.append(int(string[:-1]))
-m, count = 0, 0
-print(len(a))
-k = 0
+
+count3 = 0
+count34 = 0
+count1 = 0
 for i in range(len(a)-1):
     for j in range(i+1,len(a)):
-        k += 1
-        if (a[i] + a[j]) % 9 == 0:
-            count += 1
-            if (a[i] + a[j]) > m:
-                m = (a[i] + a[j])
-print(k, count, m)
+        count1 += 1
+        if a[i]*a[j]%34 != 0:
+            count34 += 1
+        if a[i] % 3 == 0 or a[j] % 3== 0:
+            count3 += 1
+print(count3, count1, count34)
+
+
+
+print(a[:20])
+
+
+# m, count = 0, 0
+# print(len(a))
+# k = 0
+# for i in range(len(a)-1):
+#     for j in range(i+1,len(a)):
+#         k += 1
+#         if (a[i] + a[j]) % 9 == 0:
+#             count += 1
+#             if (a[i] + a[j]) > m:
+#                 m = (a[i] + a[j])
+# print(k, count, m)
 
 
 
--- oop/02_tk.py
+++ oop/02_tk.py
@@ -1,6 +1,7 @@
 from math import sin, cos, radians, degrees
 import tkinter
 from random import choice, randint
+from time import sleep
 
 max_x = 480
 max_y = 360
@@ -15,49 +16,63 @@ class Ball:
         self.angle = angle
         self.color = color
         self.radius = 5
-        self.step = 30
-        self.Vx = self.step * cos(radians(self.angle))
-        self.Vy = self.step * sin(radians(self.angle))
-        self.avatar = canvas.create_oval(self.x, self.y, \
+        self.step = 5
+        self.dx = self.step * cos(radians(self.angle))
+        self.dy = self.step * sin(radians(self.angle))
+        self.avatar = Game.canvas.create_oval(self.x, self.y, \
             self.x + 2*self.radius, self.y + 2*self.radius, fill=self.color)
 # движение шарика
     def move(self):
-        self.x += self.Vx
-        self.y += self.Vy
+        self.x += self.dx
+        self.y += self.dy
         self.check_wall()
-        canvas.coords(self.avatar, self.x, self.y, self.x + 2*self.radius, self.y + 2*self.radius)
-        canvas.after(200,self.move)
+        print(self.x, self.y)
+        Game.canvas.coords(self.avatar, self.x, self.y, self.x + 2*self.radius, self.y + 2*self.radius)
+        # Game.canvas.after(20,self.move)
 # проверка наличия стены
     def check_wall(self):
         if self.x < 0:
             self.x = 0
-            self.Vx = -self.Vx
+            self.dx = -self.dx
         if self.x > max_x:
             self.x = max_x
-            self.Vx = -self.Vx
+            self.dx = -self.dx
         if self.y < 0:
             self.y = 0
-            self.Vy = -self.Vy
+            self.dy = -self.dy
         if self.y > max_y:
             self.y = max_y
-            self.Vy = -self.Vy
+            self.dy = -self.dy
 
 
-
-def init_main_window():
-    global root, canvas, scores_text, scores_value
+class Game:
+    scores_text = ""
+    scores_value = ""
     root = tkinter.Tk()
-    root.title("BALL")
     canvas = tkinter.Canvas(root, width=max_x, height=max_y, bg="white")
-    canvas.pack()
-    b = []
-    for i in range(10):
-        b.append(Ball(randint(0,max_x), randint(0,max_y), randint(0,359),choice(ball_available_colors)))
-    for i in range(len(b)):
-        canvas.after(20,b[i].move)
-    # canvas.bind("<Button-1>",click_event_handler)
+
+    def __init__(self):
+        Game.root.title("BALL")
+        Game.canvas.pack()
+        self.balls = []
+        for i in range(10):
+            self.balls.append(Ball(randint(0,max_x), randint(0,max_y), randint(0,359),choice(ball_available_colors)))
+        for i in range(len(self.balls)):
+            self.balls[i].move()
+        # self.root.mainloop()
+
+    def play(self):
+
+        while True:
+            sleep(0.05)
+            print(1)
+            for i in range(len(self.balls)):
+                self.balls[i].move()
+            self.root.update_idletasks()
+            self.root.update()
+
 print(__name__)
 
 if __name__ == "__main__":
-    init_main_window()
-    root.mainloop()
+    g = Game()
+    g.play()
