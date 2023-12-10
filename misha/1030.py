dc7be041b575734dfdad7c0ec3e8344055b71ca3
uncleA <lotar73@gmail.com>
2023-12-10 14:29:46 +0300

    изменения

--- misha/1030.py
+++ misha/1030.py
@@ -12,12 +12,13 @@
 # 1			     1	0
 # 1				    0
 #       1		1	0
-
-print("x y z ")
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if not((x == y) or ((y or z) <= x)):
-                print(x,y,z)
-
-
+#
+# print("x y z ")
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             if not((x == y) or ((y or z) <= x)):
+#                 print(x,y,z)
+#
+#
+print(sum([i for i in range(12)]))
