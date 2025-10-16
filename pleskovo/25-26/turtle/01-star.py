763e097d15e6386b4aea300a393d3b9ddc491806
Sergey Lyakhovetsky <lotar73@gmail.com>
2025-10-16 10:28:08 +0300

    черепаха

--- pleskovo/25-26/turtle/01-star.py
+++ pleskovo/25-26/turtle/01-star.py
@@ -12,11 +12,13 @@ def starFill(n, length):
 def star(n, length):
     t.speed(10)
     angle = n // 2 * 360 / n
+    print(n,angle)
     for i in range(n):
         t.forward(length)
         t.left(angle)
 
-for i in range(5,16,2):
+for i in range(5,22,2):
     starFill(i, 150)
-    time.sleep(2)
+    time.sleep(1
+               )
     t.reset()
