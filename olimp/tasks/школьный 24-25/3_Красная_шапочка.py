6108ee572ca857904720fe46314e3acb850667a3
lotar.ninkear <lot-work@yandex.ru>
2025-10-17 16:13:56 +0300

    Задача Красная Шапочка

--- olimp/tasks/школьный 24-25/3_Красная_шапочка.py
+++ olimp/tasks/школьный 24-25/3_Красная_шапочка.py
@@ -1,20 +1,27 @@
 def worm(index,energy):
+    print(index,energy)
     if index>=len(a):
-        return False
+        print(f"index>=len(a) index,energy=({index},{energy})")
+        return 0
     if energy - a[index] < 0:
-        return False
+        print(f"energy - a[index] < 0 ({energy} - {a[index]} )")
+        return 0
     elif index == len(a)-1:
         return energy - a[index]
-    aa = []
+    aa = [-1]
     for i in range(1, energy + 1):
         e = worm(index + i, energy - i)
-        if e:
+        print(index + i,e,aa)
+        if e>0:
             aa.append(e)
+
+    # print(aa)
     return max(aa)
 
 from random import randint
-E = 12
-a = [randint(-10, 20) if i% 7==0 else randint(-2, 5)   for i in range(10)]
+E = 6
+a = [randint(-10, 20) if i% 7==0 else randint(-2, 5)   for i in range(5)]
+print(a)
 print(a,worm(0, E))
 
 
