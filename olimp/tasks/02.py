7d83a57c58ee50a014d4d49896ac51fff1edbad9
Sergey Lyakhovetsky <lotar73@gmail.com>
2025-10-17 13:46:32 +0300

    изменения

--- olimp/tasks/02.py
+++ olimp/tasks/02.py
@@ -8,14 +8,6 @@ def foo(n,k):
     return s[::-1]
 
 
-def alhebrInSystem(a,b,system=2,action="+"):
-    
-    if action == "+":
-        n = a + b
-    elif action == "-":
-        n = abs(a - b)
-    return True
-
 while True:
     print("\n введите N и M:", end=" ")
     N,M = [int(i) for i in input().split()]    
@@ -25,7 +17,7 @@ while True:
 
     #print("Бх4 + Мх4:"+
     print(f"\n--------------M-{M}--------------")
-    for i in 4,8:
+    for i in 2,4,8:
         print(str(i)+"x"+foo(M, i),end="  ")
 
     print("\n------sum-4------dif-8--------------")
