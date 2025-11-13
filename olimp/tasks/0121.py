3b2a1879536f6e6009666f9768c89574cb575c31
lotar.ninkear <lot-work@yandex.ru>
2025-11-13 17:49:24 +0300

    +++

--- olimp/tasks/0121.py
+++ olimp/tasks/0121.py
@@ -1,25 +1,45 @@
 from random import randint
+
 n = 10
-a = [randint(1, 60) for _ in range(n)]
-n = int(input())
-a = [int(s) for s in input().split()]
+a = [randint(1, 100) for _ in range(n)]
+# n = int(input())
+# a = [int(s) for s in input().split()]
 a.sort()
+a = [0] + a + [100]
+dp = [0]+[a[i + 1] - a[i] for i in range(n - 1)]
 
-d  =  [0 for i in range(n)]
+dp_a = [0] + [1] + [0] * (n - 3) + [1]
 
-d[1] = a[1]-a[0]
-d[n-1] = a[n-1]-a[n-2]
-for i in range(2, n-2):
-    print(d)
-    if d[i]:
-        continue
-    if a[i] - a[i-1] <= a[i+1] - a[i]:
-        d[i] = a[i] - a[i-1]
+for i in range(2, n - 1):
+    dp_a[i] = dp[i - 1] + dp[i - 2]
+    if dp_a[i - 1] == 0:
+        dp_a[i] = 1
+    elif dp_a[i-2] == 1 and dp_a[i - 1] == 1 :
+        dp_a[i] = 0
     else:
-        d[i+1] = a[i+1] - a[i]
-    # if d[i] > 0 and d[i-1] > 0 and d[i-2] and i>2 > 0:
-    #     d[i-1] = 0
+        if a[i] >= a[i + 1]:
+            dp_a[i] = 0
+        elif a[i] + a[i + 2] <= a[i + 1] + a[i + 3] :
+            dp_a[i] = 0
+        else:
+            dp_a[i] = 1
+print(a, dp, dp_a, sep='\n')
 
-print(sum(d))
-print(a)
-print(d)
+# d  =  [0 for i in range(n)]
+#
+# d[1] = a[1]-a[0]
+# d[n-1] = a[n-1]-a[n-2]
+# for i in range(2, n-2):
+#     print(d)
+#     if d[i]:
+#         continue
+#     if a[i] - a[i-1] <= a[i+1] - a[i]:
+#         d[i] = a[i] - a[i-1]
+#     else:
+#         d[i+1] = a[i+1] - a[i]
+#     # if d[i] > 0 and d[i-1] > 0 and d[i-2] and i>2 > 0:
+#     #     d[i-1] = 0
+#
+# print(sum(d))
+# print(a)
+# print(d)
--- olimp/tasks/1.py
+++ olimp/tasks/1.py
@@ -1,41 +1,27 @@
-# def foo(n):
-#     if n>20:
-#         return 0
-#     if n == 11:
-#         return 0
-#     if n == 20:
-#         return 1
-#     c = 0
-#     c += foo(n+1)
-#     c+= foo(n*2)
-#     c += foo(n**2)
-#     return c
-# print(foo(2))
-#
-# print(int(bin(10)[2:]))
-arr = []
-import copy
+s = "астек"
+
+def convert(st, base):
+    st = st[::-1]
+    n=0
+    for i in range(len(st)):
+        n += s.find(st[i]) * base**i
+
+    return n
+
+print(convert("кектесак", 5) - convert("аттестат", 5))
 
 
-def foo(n, a):
-    if n < 6:
-        return 0
-    if n == 6 and 11 in a:
-        arr.append(a)
-        print(n, a, type(a))
-        return 0
-    if n > 6:
-        b = c = copy.copy(a)
-        if n - 1 >= 6:
-            a.append(n - 1)
-            foo(n - 1, a)
-        if n - 2 >= 6:
-            b.append(n - 2)
-            foo(n - 2, b)
-        if n // 3 >= 6:
-            c.append(n - 2)
-            foo(n // 3, c)
+def into(n,x1,x2):
+    return n>=x1 and n<=x2
 
+mn = 100
+for y in range(99):
+    for z in range(y,100):
+        c = 0
+        for x in range(100):
+            if into(x,27,48) <= ((not into(x,39,60) and not into(x, y, z)) <= (not into(x,27,48) and into(x,30,55)) ):
+                c += 1
+        if mn == 100:
+            mn = min(mn, z - y)
 
-foo(16, [])
-print(len(arr))
+print(mn)
